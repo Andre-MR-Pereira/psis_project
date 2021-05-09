42e4dcaf1c107342038bac99b8b642460dc5bf41
AlexaFer <alexpaint.fernandes@gmail.com>
2021-05-09 20:10:00 +0100

    user_input_not_finished

--- KVS-LocalServer.c
+++ KVS-LocalServer.c
@@ -11,12 +11,11 @@
 
 #define HASHSIZE 101
 #define SERVER_SOCKET_ADDR "/tmp/server_socket"
-#define MAX_LISTEN 2
 
 typedef struct client_list
 {
     int pid;
-    struct sockaddr_un client_socket_addr;
+    //struct sockaddr_un client_socket_addr;
     time_t connection_open;
     time_t connection_close;
     struct client_list *next;
@@ -31,9 +30,10 @@ typedef struct hash_list
 
 int client_fd_vector[10];
 int accepted_connections = 0;
-//fazer uma hash de clientes?
+int auth_server_fd = 0;
 //client_list *head_connections[10];
 hash_list *groups = NULL;
+client_list *clients = NULL;
 
 int extract_command(char *command)
 {
@@ -112,6 +112,10 @@ void create_new_group(char *group)
         aux->next = groups;
     }
     groups = aux;
+}
+
+void lookup_and_delete(char *group){
+
 }
 
 void *client_interaction(void *args)
@@ -387,6 +391,103 @@ void *client_interaction(void *args)
     }
 }
 
+int UserInput(){
+
+	char option[10]="\0", input[100]="\0", group_name[20]="\n", secret[20]="\n";
+
+    printf("Enter command (note: group names must be 100 or less characters)\n");
+	if(fgets(input, 100, stdin)==NULL){
+		return 1;
+	}
+
+	if(!sscanf(input, " %s", option)) return 1;
+
+	if(strcmp(option, "new")==0){
+
+        if(sscanf(input, " %s %s", option, group_name)==2 ){
+            //enviar msg ao AuthServer a ver se o grupo já existe (pq não podem haver grupos iguais em computadores diferentes)
+            //(basta verificar no AuthServer)
+
+            //se o grupo ainda não existir no AuthServer, então:
+            create_new_group(group_name);
+            
+            return 0;
+        }
+        else return 1;
+	}
+    else if(strcmp(option, "delete")==0){
+
+        if(sscanf(input, " %s %s", option, group_name)==2){
+            //verificar se o grupo existe antes de tentar apagar e se a lista não está vazia
+            //verificar que o grupo faz parte deste computador, pq se existe aqui, tmb existe no AuthServer (verificação local apenas)
+            hash_list *aux=groups;
+            hash_list *prev=groups;
+
+            //checks the first element of the list of groups
+            if (strcmp(group_name, aux->group) == 0){
+                //enviar msg ao AuthServer a avisar para apagar lá o grupo e segredo
+                //PRENCHEER
+
+                //deleting local info of the group:
+                free(aux->group_table); 
+                free(aux); //the same as free(groups);
+                groups=NULL; //the list of groups is empty now
+            }
+            else{
+                while(aux->next != NULL){
+                    if (strcmp(group_name, aux->group) == 0){
+                        //enviar msg ao AuthServer a avisar para apagar lá o grupo e segredo
+                        //PRENCHEER
+
+                        //deleting local info of the group:
+                        prev->next=aux->next;
+                        free(aux->group_table); 
+                        free(aux);
+                        break;
+                    }
+                    prev=aux;
+                    aux=aux->next;
+                }
+            }
+
+            printf("That group doesn't exist\n"); 
+
+            return 0;
+        }
+        else return 1;
+
+    }
+    else if(strcmp(option, "group")==0){
+
+        if(sscanf(input, " %s %d", option, group_name)==2){
+            //verificar se o grupo existe
+            
+
+            return 0;
+        }
+        else return 1;
+
+    }
+    else if(strcmp(option, "status")==0){
+        //list all currently and past connected applications, with the following info: client PID, conect_time and close_time (se for o caso)
+        return 0;
+    }  
+    
+    //if the command is invalid:
+    return 1;
+}
+
+void *user_interface(void *args){
+
+    while(1){
+        //a user interface vai precisar de receber o fd do AuthServer! (mudar depois)
+        if(UserInput()) {
+            printf("Invalid command\n");
+        }
+    }
+
+}
+
 int main()
 {
     int server_socket, buffer;
@@ -410,7 +511,7 @@ int main()
         exit(-1);
     }
 
-    if (listen(server_socket, MAX_LISTEN) == -1)
+    if (listen(server_socket, 2) == -1)
     {
         perror("listen");
         exit(-1);
@@ -420,6 +521,12 @@ int main()
     int i = 0;
     int client_size = sizeof(client_socket_addr);
     char buf[20];
+
+    if (pthread_create(&t_id[i], NULL, user_interface, NULL) != 0){
+        printf("Error on thread creation");
+    }
+    i++;
+
     while (1)
     {
         int client_fd = accept(server_socket, (struct sockaddr *)&client_socket_addr, &client_size);
@@ -435,6 +542,8 @@ int main()
         struct tm *tm;
 
         time(&start);
+        clients->connection_open = localtime(&start);
+        /*//if conversion is necessary, use this code:
         tm = localtime(&start);
         if (strftime(buf, sizeof(buf), "%T %D", tm) == 0)
         {
@@ -443,10 +552,10 @@ int main()
         else
         {
             printf("start time: %s %ld\n", buf, strlen(buf));
-        }
-
+        }*/
+        
         //guardar pid da pessoa (funcao ja esta criada)
-        if (pthread_create(&t_id[i], NULL, client_interaction, (void *)&client_fd) != 0)
+        if ((pthread_create(&t_id[i], NULL, client_interaction, (void *)&client_fd) != 0))
         {
             printf("Error on thread creation");
         }
