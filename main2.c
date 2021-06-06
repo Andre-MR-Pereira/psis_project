2b2be6306e22c8fc1a29ce42546ef7dc1f6e1aea
AlexaFer <alexpaint.fernandes@gmail.com>
2021-06-06 04:45:56 +0100

    main with operations out of order for testing

---
+++ main2.c
@@ -0,0 +1,35 @@
+#include "hash.h"
+#include "KVS-lib.h"
+#include <stdio.h>
+
+void callback(char *changed_key)
+{
+    printf("The key with name %s was changed\n", changed_key);
+}
+
+int main()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        
+        put_value("Laranja", "39875983759236589243659237392856723");
+        put_value("Mabeco", "5");
+        put_value("Congresso", "200");
+        establish_connection("grupoteste", "12345");
+        char *buffer;
+        get_value("Laranja", &buffer);
+        printf("Voltou %s\n", buffer);
+        delete_value("Mabeco");
+        register_callback("Laranja", callback);
+        register_callback("Congresso", callback);
+        register_callback("Laranja", callback);
+        printf("------------------\n");
+        put_value("Congresso", "500");
+        delete_value("Congresso");
+        delete_value("Laranja");
+        close_connection();
+        printf("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&    %d  &&&&&&&&&&&&&&&&&&&&&&&&&\n", i);
+    }
+    printf("leaving...\n");
+    return 0;
+}
